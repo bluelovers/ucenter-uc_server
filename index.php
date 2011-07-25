4c9e5c48bb76a7f393595fccbcde5ea66808be93
bluelovers <codelovers@users.sourceforge.net>
2011-07-25 18:24:56 +0800

    bugfix

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ if(file_exists(UC_ROOT.RELEASE_ROOT.'model/base.php')) {
 }
 
 if(
-	in_array($m, array('sc') ||
+	in_array($m, array('sc')) ||
 	in_array($m, array('app', 'frame', 'user', 'pm', 'pm_client', 'tag', 'feed', 'friend', 'domain', 'credit', 'mail', 'version'))) {
 
 	if(file_exists(UC_ROOT.RELEASE_ROOT."control/$m.php")) {
